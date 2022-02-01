b726e0ccf90c972c5d7f7c017945b891f14c47a5
Caio <caiofmart@gmail.com>
2022-01-31 22:54:22 -0300

    Initial Commit

---
+++ AddView.swift
@@ -0,0 +1,55 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Caio Feitoza Martins on 16/07/21.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    @State private var showingAlert = false
+    @State private var messageForAlert = ""
+
+    static let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                }   else {
+                    self.messageForAlert = self.amount
+                    self.showingAlert = true
+                }
+            })
+        }.alert(isPresented: $showingAlert) {   () -> Alert in
+            Alert(title: Text("Invalid amount"), message: Text("Please enter a valid number"), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
